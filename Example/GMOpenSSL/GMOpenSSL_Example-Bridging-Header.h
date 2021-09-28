39607555b4f32fd520d6245ce54348f53017debf
lifei <lifei_zdjl@126.com>
2021-09-28 18:02:20 +0800

    SPM

--- Example/GMOpenSSL/GMOpenSSL_Example-Bridging-Header.h
+++ Example/GMOpenSSL/GMOpenSSL_Example-Bridging-Header.h
@@ -2,3 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "openssl/md5.h"
+//#include <openssl/md5.h>
--- Example/Pods/Target Support Files/GMOpenSSL/GMOpenSSL-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GMOpenSSL : NSObject
-@end
-@implementation PodsDummy_GMOpenSSL
-@end
