ed602918a662f4308a3da62294c4cb28c2db9d7b
lifei <lifei_zdjl@126.com>
2021-09-28 16:48:20 +0800

    remove arm64e.

--- OpenSSL_BUILD/build-libssl.sh
+++ OpenSSL_BUILD/build-libssl.sh
@@ -29,7 +29,7 @@ DEFAULTVERSION="1.1.1l"
 
 # Default (=full) set of targets to build
 # ios-sim-cross-x86_64 ios64-cross-arm64 ios64-cross-arm64e ios-cross-armv7 ios-cross-armv7s
-DEFAULTTARGETS="ios-sim-cross-x86_64 ios64-cross-arm64 ios64-cross-arm64e"  # mac-catalyst-x86_64 is a valid target that is not in the DEFAULTTARGETS because it's incompatible with "ios-sim-cross-x86_64"
+DEFAULTTARGETS="ios-sim-cross-x86_64 ios64-cross-arm64"  # mac-catalyst-x86_64 is a valid target that is not in the DEFAULTTARGETS because it's incompatible with "ios-sim-cross-x86_64"
 
 # Minimum iOS/tvOS SDK version to build for
 IOS_MIN_SDK_VERSION="9.0"
